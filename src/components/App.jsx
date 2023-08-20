910ff6bac0dd3fb455da83f294307266844de645
soberjan1 <aliguseynov48@gmail.com>
2023-08-20 13:22:53 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route></Route>
+      </Routes>
     </div>
   );
 };
